21090c5052d05740e916c340bd906ee6ed3d4c00
Soham Roy <sohamroy52@gmail.com>
2025-05-05 09:48:08 +0530

    Now the crash can happen when the car collides with the object.

--- main.py
+++ main.py
@@ -98,6 +98,13 @@ def game_loop():
             thing_starty = 0 - thing_height
             thing_startx = random.randrange(0, display_width-thing_width)
 
+        if y < thing_starty+thing_height:
+            print('Y crossover')
+            if x > thing_startx and x < thing_startx + thing_width or x + car_width > thing_startx and x + car_width < thing_startx + thing_width:
+                print('X crossoveer')
+                crashed()
+
+
         pygame.display.update()
         clock.tick(120)
 
